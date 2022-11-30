1fe9be01d02a4fbe5fdd451ae67dd6a927c9353c
ajanders <ajanders@uw.edu>
2022-11-29 21:05:24 -0800

    bug fixes, new figures

--- MAIN_Python.py
+++ MAIN_Python.py
@@ -14,85 +14,202 @@ This works with Python 3.7.6.
 
 # %% Import packages
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import scipy.signal as sg
 
-# %% Setup the participant and file
+# %% Define data location
 
 # get the name of the current directory
 directory = os.getcwd()
 
+
 # parquet data directory
-data_dir = directory+'\\Parquet Data'
+root = directory+'\\Parquet Data'
+
+# %% Figure 1: Subject AB06 IK/ID on levelground
+
+### Define file, subject, and condition ###
 
 # subject
-subject = 'AB12'
-date = "11_04_2018"
+subject = 'AB06'
+date = "10_09_18"
+
+# conditon
+condition = 'levelground'
+
+# file to load
+file = 'levelground_ccw_normal_01_01.parquet'
+
+### Load data as pandas dataframes ###
+
+# load inverse kinematics data
+sensor = 'ik'
+ik_path = root+'\\'+subject+'\\'+date+'\\'+condition+'\\'+sensor+'\\'+file
+ik_data = pd.read_parquet(ik_path, engine='pyarrow')
+
+# load metadata describing activity labels
+sensor = 'conditions'
+label_path = root+'\\'+subject+'\\'+date+'\\'+condition+'\\'+sensor+'\\'+file
+label_data = pd.read_parquet(label_path, engine='pyarrow')
+
+### Extract signals from dataframes ###
+
+# get joint angles
+t1 = ik_data['Header']
+ankle_angle = ik_data['ankle_angle_r']
+knee_angle = ik_data['knee_angle_r']
+
+# get labels
+t0 = label_data['Header']
+label = label_data['Label']
+
+### Create a subplot ###
+
+fig, axes = plt.subplots(3, 1, sharex=True)
+
+axes[0].plot(t0, label, '#4357AD')
+axes[0].grid()
+axes[0].set_ylabel('Label')
+axes[0].set_title(subject+', '+file)
+
+axes[1].plot(t0, ankle_angle, '#48A9A6')
+axes[1].grid()
+axes[1].set_ylabel('Ankle Angle (deg)')
+
+axes[2].plot(t1, -knee_angle, '#C1666B')
+axes[2].grid()
+axes[2].set_ylabel('Knee Angle (deg)')
+axes[2].set_xlabel('Time (s)')
+
+# %% Figure 2: Subject AB15 shank IMU on treadmill
+
+### Define file, subject, and condition ###
+
+# subject
+subject = 'AB15'
+date = "11_07_2018"
 
 # conditon
 condition = 'treadmill'
 
 # file to load
-file = 'treadmill_02_01.parquet'
+file = 'treadmill_05_01.parquet'
 
-# %% Load data as pandas dataframes
-
-# load ground reaction force data
-sensor = 'fp'
-grf_path = data_dir+'\\'+subject+'\\'+date+'\\'+condition+'\\'+sensor+'\\'+file
-grf_data = pd.read_parquet(grf_path, engine='pyarrow')
+### Load data as pandas dataframes ###
 
 # load inverse kinematics data
-sensor = 'ik'
-ik_path = data_dir+'\\'+subject+'\\'+date+'\\'+condition+'\\'+sensor+'\\'+file
-ik_data = pd.read_parquet(ik_path, engine='pyarrow')
+sensor = 'imu'
+imu_path = root+'\\'+subject+'\\'+date+'\\'+condition+'\\'+sensor+'\\'+file
+imu_data = pd.read_parquet(imu_path, engine='pyarrow')
 
-# load inverse dynamics data
-sensor = 'id'
-id_path = data_dir+'\\'+subject+'\\'+date+'\\'+condition+'\\'+sensor+'\\'+file
-id_data = pd.read_parquet(id_path, engine='pyarrow')
-
-# load metadata describing treadmill speed
+# load metadata describing activity labels
 sensor = 'conditions'
-speed_path = data_dir+'\\'+subject+'\\'+date+'\\'+condition+'\\'+sensor+'\\'+file
-speed_data = pd.read_parquet(speed_path, engine='pyarrow')
+label_path = root+'\\'+subject+'\\'+date+'\\'+condition+'\\'+sensor+'\\'+file
+label_data = pd.read_parquet(label_path, engine='pyarrow')
 
-# %% Extract signals from dataframes
+### Extract signals from dataframes ###
 
-# get ground reaction force data
-t0 = grf_data['Header']
-Fy = grf_data['Treadmill_R_vy']
+# get imu signals
+t1 = imu_data['Header']
+ax = imu_data['shank_Accel_X']
+ay = imu_data['shank_Accel_Y']
+az = imu_data['shank_Accel_Z']
+gx = imu_data['shank_Gyro_X']
+gy = imu_data['shank_Gyro_Y']
+gz = imu_data['shank_Gyro_Z']
 
-# get ankle angle
-t1 = ik_data['Header']
-ankle_angle = ik_data['ankle_angle_r']
+# get belt speed
+t0 = label_data['Header']
+label = label_data['Speed']
 
-# get ankle torque
-t2 = id_data['Header']
-ankle_moment = id_data['ankle_angle_r_moment']
+### Create a subplot ###
 
-# get treadmill speed
-t3 = speed_data['Header']
-speed = speed_data['Speed']
+fig, axes = plt.subplots(3, 1, sharex=True)
 
-# %% Create a subplot
-
-fig, axes = plt.subplots(4, 1, sharex=True)
-
-axes[0].plot(t3, speed, '#118AB2')
+axes[0].plot(t0, label, 'k')
 axes[0].grid()
-axes[0].set_ylabel('Speed (m/s)')
+axes[0].set_ylabel('Treadmill Speed (m/s)')
+axes[0].set_title(subject+', '+file)
 
-axes[1].plot(t0, Fy, '#EF476F')
+axes[1].plot(t1, ax, '#E8C547')
+axes[1].plot(t1, ay, '#124E78')
+axes[1].plot(t1, az, '#A63A50')
 axes[1].grid()
-axes[1].set_ylabel('Ground Reaction Force (N)')
+axes[1].set_ylabel('Shank Accel (g)')
 
-axes[2].plot(t1, ankle_angle, '#FFD166')
+axes[2].plot(t1, gx, '#E8C547')
+axes[2].plot(t1, gy, '#124E78')
+axes[2].plot(t1, gz, '#A63A50')
 axes[2].grid()
-axes[2].set_ylabel('Ankle Angle (deg)')
+axes[2].set_ylabel('Shank Gyro (rad/s)')
+axes[2].set_xlabel('Time (s)')
 
-axes[3].plot(t2, ankle_moment, '#06D6A0')
-axes[3].grid()
-axes[3].set_ylabel('Ankle Moment (Nm)')
-axes[3].set_xlabel('Time (s)')
+# %% Figure 3: Subject AB30 on stairs
+
+### Define file, subject, and condition ###
+
+# subject
+subject = 'AB30'
+date = "03_09_2019"
+
+# conditon
+condition = 'stair'
+
+# file to load
+file = 'stair_1_r_01_01.parquet'
+
+### Load data as pandas dataframes ###
+
+# load inverse kinematics data
+sensor = 'emg'
+emg_path = root+'\\'+subject+'\\'+date+'\\'+condition+'\\'+sensor+'\\'+file
+emg_data = pd.read_parquet(emg_path, engine='pyarrow')
+
+# load metadata describing activity labels
+sensor = 'markers'
+marker_path = root+'\\'+subject+'\\'+date+'\\'+condition+'\\'+sensor+'\\'+file
+marker_data = pd.read_parquet(marker_path, engine='pyarrow')
+
+# extract emg signals
+t1 = emg_data['Header']
+soleus = emg_data['soleus']
+glute_med = emg_data['gluteusmedius']
+
+soleus_rect = np.abs(soleus)
+glute_med_rect = np.abs(glute_med)
+
+b, a = sg.butter(4, 5/500)
+sol_env = sg.filtfilt(b, a, soleus_rect)
+glute_med_env = sg.filtfilt(b, a, glute_med_rect)
+
+# get markers
+t0 = marker_data['Header']
+r_heel_y = marker_data['R_Heel_y']
+r_toe_y = marker_data['R_Toe_Tip_y']
+
+### Create a subplot ###
+
+fig, axes = plt.subplots(3, 1, sharex=True)
+
+axes[0].plot(t0, r_heel_y, '#445E93', label='heel')
+axes[0].plot(t0, r_toe_y, '#524632', label='toe')
+axes[0].grid()
+axes[0].set_ylabel('Height (mm)')
+axes[0].legend()
+axes[0].set_title(subject+', '+file)
+
+axes[1].plot(t1, soleus, '#86CB92', label='raw')
+axes[1].plot(t1, sol_env, 'k', label='linear envelope')
+axes[1].grid()
+axes[1].legend()
+axes[1].set_ylabel('Soleus (mV)')
+
+axes[2].plot(t1, glute_med, '#FE654F', label='raw')
+axes[2].plot(t1, glute_med_env, 'k', label='linear envelope')
+axes[2].grid()
+axes[2].set_ylabel('Glute Med (mV)')
+axes[2].legend()
+axes[2].set_xlabel('Time (s)')
--- README.md
+++ README.md
@@ -1,30 +1,54 @@
 # convert-ga-tech-walking-data
 
-The Epic Lab at Georgia Tech created a really nice open-source database of gait data collected from healthy subjects walking in a variety of conditions. The details of the data and collection procedures can be found in:
+The Epic Lab at Georgia Tech created a really nice open-source database of gait data collected from healthy subjects walking in a variety of conditions.
+The details of the data and collection procedures can be found in:
 
 [Camargo et al., A comprehensive, open-source dataset of lower limb
 biomechanics in multiple conditions of stairs, ramps, and level-ground
 ambulation and transitions., Journal of Biomechanics, 2021.](https://www.sciencedirect.com/science/article/pii/S0021929021001007)
 
 I've found myself wanting to use this dataset for a variety of projects, but as all of the files are stored as .mat files, I've been unable to load
-them into Python (my scientific computing language of choice). [.mat files](https://www.mathworks.com/help/matlab/import_export/mat-file-versions.html) are a proprietary binary file format created by MathWorks for use with Matlab. It seems like there should be a way to load a .mat file into Python. There is a [function in the scipy package](https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.loadmat.html) that supposedly loads .mat files, but I couldn't make it load anything intelligible.
+them into Python (my scientific computing language of choice). [.mat files](https://www.mathworks.com/help/matlab/import_export/mat-file-versions.html) are
+a proprietary binary file format created by MathWorks for use with Matlab. It seems like there should be a way to load a .mat file into Python. There is a
+[function in the scipy package](https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.loadmat.html) that supposedly loads .mat files, but I couldn't
+make it load anything intelligible.
 
-Instead, I decided to write a Matlab script (and set of functions) that copies the entire dataset over to another open-source file format that is compatible with Matlab, Python, and other programing languages. The file format that I've selected is Apache Parquet, which is optimized for columnar data. Details are here:
+Instead, I decided to write a Matlab script (and set of functions) that copies the entire dataset over to another open-source file format that is compatible
+with Matlab, Python, and other programing languages. The file format that I've selected is Apache Parquet, which is optimized for columnar data. Details are here:
 
 https://parquet.apache.org/
 
 ## Matlab Code
 
-The conversion script (MAIN_Matlab.m) is pretty straightforward. It assumes that you've got the database in this directory, inside of a folder titled 'matlab data', where each subfolder is a participant's data folder. This is the same way the data is organzied when you download it from the [Epic Lab website](https://www.epic.gatech.edu/opensource-biomechanics-camargo-et-al/). You'll need to unzip each participant folder (or you could modify this code to unzip them for you). When you run this script, it will create another subdirectory called 'parquet data' that mirrors the matlab data directory and will fill each folder data. The only differences is that instead of each file being a '.mat' file, it's a '.parquet' file. Parquet files can be read by Matlab and by Python via the Pandas package. The main matlab script calls custom functions in the 'matlab functions' folder. 
+The conversion script (MAIN_Matlab.m) is pretty straightforward. It assumes that you've got the database in this directory, inside of a folder titled 'matlab data', where each
+subfolder is a participant's data folder. This is the same way the data is organzied when you download it from the
+[Epic Lab website](https://www.epic.gatech.edu/opensource-biomechanics-camargo-et-al/). You'll need to unzip each participant folder (or you could modify this code to unzip them
+for you). When you run this script, it will create another subdirectory called 'parquet data' that mirrors the matlab data directory and will fill each folder data. The only
+differences is that instead of each file being a '.mat' file, it's a '.parquet' file. Parquet files can be read by Matlab and by Python via the Pandas package. The main matlab
+script calls custom functions in the 'matlab functions' folder. 
 
-The GA Tech database uses the date of the data collection as the name of one of the high level folder names. I've gathered the dates for each participant in the file 'subject_date_key.csv', and one of the functions uses this file to create the folder name.
+The GA Tech database uses the date of the data collection as the name of one of the high level folder names. I've gathered the dates for each participant in the file
+'subject_date_key.csv', and one of the functions uses this file to create the folder name.
 
-The Main_Matlab.m script takes a really long time to run. For all 22 participants, it took my powerful desktop computer ~6 hours. I recommend running participants in batches by changing the names in the 'subjects' array in this script.
+The Main_Matlab.m script takes a really long time to run. For all 22 participants, it took my powerful desktop computer ~6 hours. I recommend running participants in batches by
+changing the names in the 'subjects' array in this script.
+
+The script "verification_plots.m" creates three subplots of the matlab data that can be compared to data from the converted parquet database to verify that the conversion worked.
+These figures are shown below. 
 
 ## Python Code
 
-I've also written a simple Python script that loads and plots some of the converted data for verification (MAIN_Python.py). The data is from one subject walking on a treadmill that increases in speed partway through the trial. I've plotted treadmill speed, ground reaction force, right ankle angle, and right ankle moment. Here is the (zoomed/adjusted) plot that is generated by the Python script:
+I've also written a simple Python script that loads and plots the same three files from the parquet database (MAIN_Python.py). The figures from each database show good agreement
+and are shown below:
 
-<img src="python_script_output.png" width=500>
+### AB06 Levelground Walking
+
+Matlab:
+
+<img src="img/AB06_levelground_ccw_normal_01_01_matlab.png" width=500>
+
+Python:
+
+<img src="img/AB06_levelground_ccw_normal_01_01_python.png" width=500>
 
 Looks good to me! Hopefully this is useful for someone else out there. 
